7a3c047db4507cb0baac395c4f82ad05ab43ea74
Brian M <b.a.munjoma@gmail.com>
2023-11-08 17:10:06 +0000

    Create ViewController when running Unit Tests

---
+++ App Tests/TestingDelegate/TestingSceneDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  TestingSceneDelegate.swift
+//  App Tests
+//
+//  Created by Brian Munjoma on 08/11/2023.
+//
+
+import UIKit
+
+class TestingSceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = TestingRootViewController()
+        window?.makeKeyAndVisible()
+    }
+}
