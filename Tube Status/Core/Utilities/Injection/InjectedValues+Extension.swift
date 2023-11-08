d45facd23a8ecd3313b8715299bd37ee8fabcf41
Brian M <b.a.munjoma@gmail.com>
2023-11-08 15:09:28 +0000

    Support APP_KEY injection through Config file

--- Tube Status/Core/Utilities/Injection/InjectedValues+Extension.swift
+++ Tube Status/Core/Utilities/Injection/InjectedValues+Extension.swift
@@ -22,13 +22,17 @@ private struct EnvironmentProviderKey: InjectionKey {
     static var currentValue: EnvironmentType = computedEnvironment
 
     static var computedEnvironment: EnvironmentType {
-            // TODO: Move App_Key to Config file
-            let item = URLQueryItem(name: "app_key", value: "6fedd8b313f74f3cb750b9ecc26af642")
-            return EnvironmentInfo(scheme: .secure,
+        // TODO: Move App_Key to Config file
+        guard let key = Bundle.main.infoDictionary?["APP_KEY"] as? String, key.count > 0 else {
+            fatalError("App Key not supplied")
+        }
+        let item = URLQueryItem(name: "app_key", value: key)
+        print("Using key ending with \(key.suffix(5))")
+        return EnvironmentInfo(scheme: .secure,
                                endpoint: "api.tfl.gov.uk",
                                additionalHeaders: [:],
                                secret: .queryItem(item))
-        }
+    }
 }
 
 // MARK: - Injected Values
