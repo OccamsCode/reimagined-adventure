813df255745d8f516e4b449b7d7110a4339a223d
Brian M <b.a.munjoma@gmail.com>
2023-11-09 17:28:56 +0000

    Fixing memory management issue

--- Tube Status/Features/Tube Status/Views/TubeStatusTableViewController.swift
+++ Tube Status/Features/Tube Status/Views/TubeStatusTableViewController.swift
@@ -19,7 +19,7 @@ class TubeStatusTableViewController: UITableViewController {
         tableView.separatorColor = .label
         tableView.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
         
-        viewModel.fetchAllStatus {
+        viewModel.fetchAllStatus { [unowned self] in
             DispatchQueue.main.async {
                 self.tableView.reloadData()
             }
